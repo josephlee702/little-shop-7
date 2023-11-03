1dcb08426ed8bc1779ae7e81fd19177b022f7df6
Nathan Trautenberg <Natester123@mac.com>
2023-11-03 11:08:37 -0600

    need to pull updated invoiceitem method before tacklingus3 query

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -5,5 +5,7 @@ class MerchantsController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
+    @top = @merchant.top_five_merchant_customers_by_transaction
+    #require 'pry'; binding.pry
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,17 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :items
 
   validates :name, presence: true
+
+  def top_five_merchant_customers_by_transaction
+    Customer
+    .joins("JOIN invoices ON invoices.customer_id = customers.id")
+    .joins("JOIN transactions ON transactions.invoice_id = invoices.id")
+    .joins("JOIN invoice_items ON invoice_items.items_id = items.id")
+    .joins("JOIN invoice_items ON invoice_items.invoice_id = invoice.id")
+    .where("transactions.result = 1 AND invoices.id IN (SELECT invoice_id FROM items WHERE items.merchant_id = ?)", 9)
+    .group("customers.id")
+    .select("customers.*, COUNT(transactions.id) AS transaction_count")
+    .order("transaction_count DESC")
+    .limit(5)
+  end
 end
